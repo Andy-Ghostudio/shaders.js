09893bd7e154dd81e778596f73a649c97b8e823a
Andy-Ghostudio <andy@ghostudio.design>
2025-02-11 02:03:47 +0000

    Update shaders.js

--- shaders.js
+++ shaders.js
@@ -48,7 +48,7 @@ void main() {
 	pVel -= 0.005 * delta * pressure;
 
 	pVel *= 1.0 - 0.002 * delta;
-	pressure *= 0.2 // was .999;
+	pressure *= 0.8 // was .999;
 
 	vec2 mouseUV = mouse / resolution;
 	if(mouse.x > 0.0) {
